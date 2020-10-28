9ab37c45af3f6d227adbb511ecc332d657fcb044
jjpatric <jjpatric@ualberta.ca>
2020-10-27 18:17:57 -0600

    Added failing test case comment

--- app/src/test/java/com/example/simpleparadox/listycity/CityListTest.java
+++ app/src/test/java/com/example/simpleparadox/listycity/CityListTest.java
@@ -52,6 +52,7 @@ class CityListTest {
     void testGetCities() {
         CityList cityList = mockCityList();
 
+        // This is the test that I changed
         assertEquals(1, mockCity().compareTo(cityList.getCities().get(0)));
 
         City city = new City("Charlottetown", "Prince Edward Island");
